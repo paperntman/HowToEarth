37dfb33d05bf08685e7b67a2fc1d25fd8bbe7d1d
paperntman <kdhwmanse@gmail.com>
2023-06-24 12:46:02 +0900

    initial

---
+++ src/main/java/topen/khjs/howtoearth/HowToEarthApplication.java
@@ -0,0 +1,15 @@
+package topen.khjs.howtoearth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import static java.lang.System.*;
+
+@SpringBootApplication
+public class HowToEarthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowToEarthApplication.class, args);
+    }
+
+}
---
+++ src/test/java/topen/khjs/howtoearth/HowToEarthApplicationTests.java
@@ -0,0 +1,14 @@
+package topen.khjs.howtoearth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HowToEarthApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.print("Hello. WOrld!");
+    }
+
+}
